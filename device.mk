0875323bfd4effe41826d0f3674caf2afe030a28
Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2026-05-30 15:44:49 +0800

    Reapply "aston: Move back to qti vibrator service"
    This reverts commit 4536d96be6f00e278ef5196a1297831ba12be76a.
    
    Signed-off-by: Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -94,7 +94,9 @@ $(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,USE_OPLUSTOUCH,true)
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator.service.oplus-richtap
+    vendor.qti.hardware.vibrator.service.oplus
+
+$(call soong_config_set_bool,OPLUS_LINEAGE_VIBRATOR_HAL,USE_EFFECT_STREAM,true)
 
 # Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/oneplus/sm8550-common/common.mk)
