eea1aa2af89d665c3e5f05bc7206dd17329ef792
Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2026-08-02 23:18:32 +0800

    [HACK] aston: extras: force enter bypass mode of iris chip on AOD
    Fixes FOD glitches due to refresh rate change.
    
    Signed-off-by: Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- OPlusExtras/src/org/lineageos/settings/MEMC/MemcService.java
+++ OPlusExtras/src/org/lineageos/settings/MEMC/MemcService.java
@@ -4,9 +4,13 @@ import android.app.Service;
 import android.app.ActivityTaskManager;
 import android.app.ActivityTaskManager.RootTaskInfo;
 import android.app.IActivityTaskManager;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.display.DisplayManager;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.content.Intent;
+import android.os.SystemProperties;
+import android.view.Display;
 
 public class MemcService extends Service {
 
@@ -15,6 +19,7 @@ public class MemcService extends Service {
     private String mPreviousApp = "";
     private MemcUtils mMemcUtils;
     private IActivityTaskManager mActivityTaskManager;
+    private DisplayManager mDisplayManager;
 
     @Override
     public void onCreate() {
@@ -24,6 +29,12 @@ public class MemcService extends Service {
         } catch (RemoteException e) {
             // ignore
         }
+
+        mDisplayManager = (DisplayManager) getSystemService(Context.DISPLAY_SERVICE);
+        if (mDisplayManager != null) {
+            mDisplayManager.registerDisplayListener(mDisplayListener, null);
+        }
+
         mMemcUtils = new MemcUtils(this);
         mMemcUtils.executeDefaultConfig();
         super.onCreate();
@@ -35,9 +46,40 @@ public class MemcService extends Service {
     }
 
     @Override
+    public void onDestroy() {
+        if (mDisplayManager != null) {
+            mDisplayManager.unregisterDisplayListener(mDisplayListener);
+        }
+        super.onDestroy();
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+    private final DisplayManager.DisplayListener mDisplayListener = new DisplayManager.DisplayListener() {
+        @Override
+        public void onDisplayAdded(int displayId) {
+        }
+
+        @Override
+        public void onDisplayRemoved(int displayId) {
+        }
+
+        @Override
+        public void onDisplayChanged(int displayId) {
+            Display display = mDisplayManager != null ? mDisplayManager.getDisplay(displayId) : null;
+            if (display == null || displayId != Display.DEFAULT_DISPLAY) {
+                return;
+            }
+
+            int state = display.getState();
+            if (state == Display.STATE_DOZE || state == Display.STATE_DOZE_SUSPEND) {
+                SystemProperties.set("sys.oplus.iris.cmd", "56 1 1");
+            }
+        }
+    };
 
     private final android.app.TaskStackListener mTaskListener = new android.app.TaskStackListener() {
         @Override
